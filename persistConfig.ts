d0c684cb13b732dda4c81959d30a649f1de27bdd
Sergey Primakovich <prima.psa@gmail.com>
2024-05-20 23:42:06 +0300

    chore: persist config

--- persistConfig.ts
+++ persistConfig.ts
@@ -1,10 +1,10 @@
 // @ts-ignore
 import storage from 'redux-persist/lib/storage';
+import { setGenres } from '@/store/slices/filterSlice';
 
 const persistConfig = {
   key: 'root',
   storage,
-  whitelist: [],
 };
 
 export default persistConfig;
