44ca6e6333a44e10fcac94239069c3dc0644eced
Sergey Primakovich <prima.psa@gmail.com>
2024-05-24 16:39:58 +0300

    feat: store

--- src/store/index.ts
+++ src/store/index.ts
@@ -7,28 +7,27 @@ import { moviesSlice } from '@/store/slices/moviesSlice';
 import { filterSlice } from '@/store/slices/filterSlice';
 import { genresSlice } from '@/store/slices/genresSlice';
 import { movieSlice } from '@/store/slices/movieSlice';
+import { ratedSlice } from '@/store/slices/ratedSlice';
 
+const persistedReducer = persistReducer(persistConfig, ratedSlice.reducer);
 const rootReducer = combineReducers({
   movies: moviesSlice.reducer,
   filter: filterSlice.reducer,
   genres: genresSlice.reducer,
   movie: movieSlice.reducer,
+  rated: persistedReducer,
 });
 
-//const persistedReducer = persistReducer(persistConfig, rootReducer);
-//ts-ignore
 const store = configureStore({
-  //reducer: persistedReducer,
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(thunk),
 });
 
-//const persistor = persistStore(store);
+const persistor = persistStore(store);
 
-//export { store, persistor };
-export { store };
 export const useAppDispatch: () => DispatchType = useDispatch;
 export type RootStateType = ReturnType<typeof rootReducer>;
 export const useAppSelector: TypedUseSelectorHook<RootStateType> = useSelector;
 export type AppDispatchType = any | typeof store.dispatch;
 export type DispatchType = typeof store.dispatch;
+export { store, persistor };
