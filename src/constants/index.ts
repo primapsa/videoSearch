c2ca28897330640fefcc0f2f91ccff052cf17753
Sergey Primakovich <prima.psa@gmail.com>
2024-05-24 16:40:09 +0300

    feat: const

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,5 @@
+import { BreadcrumpsType } from '@/types';
+
 export const TYPE = {
   MOVIES: 'movies',
   MOVIE: 'movie',
@@ -7,11 +9,14 @@ export const RATING = {
   from: 1,
   to: 10,
 };
-export const API_BASE_URL = 'https://proxy-psa.vercel.app/prx/';
+const API_BASE = 'https://proxy-psa.vercel.app/';
+export const API_BASE_URL = `${API_BASE}prx/`;
+export const IMAGE_HOST = `${API_BASE}/img/w500`;
+
 export const STATUSES = {
   OK: 200,
 };
-
+export const GENRES_LIMIT = 3;
 export const APP_STATUSES = {
   LOADING: 'loading',
   ERROR: 'error',
@@ -41,8 +46,24 @@ export const PAGINATE = {
   MARGIN: 0,
 };
 export const PAGE = {
-  INIT: 0,
+  INIT: 1,
 };
 export const ERRORS = {
   FETCH: 'fetch error!',
 };
+
+export const BREADCRUMBS: BreadcrumpsType[] = [{ title: 'Movies', href: '../' }];
+export const TRAILER_TYPE = {
+  TRAILER: 'Trailer',
+};
+export const ITEM_PER_PAGE = 4;
+export const API_ITEM_PER_PAGE = 20;
+export const PATH = {
+  MOVIE: 'movie/',
+};
+export const NUMBER_RATING = {
+  MIN: 1,
+  MAX: 10,
+  STEP: 1,
+};
+export const VIDEO_URL = 'https://www.youtube.com/watch?v=';
