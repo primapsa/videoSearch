727e2afea35df6b3b304bdd4cbfee9dd3c5bba62
Sergey Primakovich <prima.psa@gmail.com>
2024-05-24 16:45:21 +0300

    refactor: config

--- src/components/vote/vote.tsx
+++ src/components/vote/vote.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { MouseEvent } from 'react';
 import { Group, Rating as RatingMantine, Text } from '@mantine/core';
 import { MovieCardProps } from '@/types';
+import s from './styles.module.scss';
 
 const Vote = ({ vote = 0, onVote, id }: Pick<MovieCardProps, 'vote' | 'onVote' | 'id'>) => {
-  const ratingClickHandler = () => {
+  const ratingClickHandler = (e: MouseEvent<HTMLDivElement>) => {
+    e.preventDefault();
     onVote && onVote(id);
   };
   return (
-    <Group>
-      <RatingMantine count={1} readOnly onClick={ratingClickHandler} />
-      {Boolean(vote) && <Text span>{vote}</Text>}
+    <Group className={s.container}>
+      <RatingMantine
+        count={1}
+        onClick={ratingClickHandler}
+        defaultValue={vote}
+        color="violet"
+        size="30px"
+      />
+      {Boolean(vote) && <Text className={s.text}>{vote}</Text>}
     </Group>
   );
 };
